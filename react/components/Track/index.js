4093c793548c25d0366d967dfd6348f37aa8d93f
Pavel Sotnikov <pavel@alterbooks.ru>
2017-03-04 04:00:50 +0500

    update: предусматриваем ситуацию с возможной ошибкой

--- react/components/Track/index.js
+++ react/components/Track/index.js
@@ -8,14 +8,24 @@ export default class Track extends React.Component {
     }
 
     render() {
-        return (
-            <tr>
-                <td>{this.props.track.name}</td>
-                <td>{this.props.track.singer}</td>
-                <td>{this.props.track.genre}</td>
-                <td>{this.props.track.year}</td>
-            </tr>
-        );
+        let track = null;
+        try {
+            track =
+                <tr>
+                    <td>{this.props.track.name}</td>
+                    <td>{this.props.track.singer}</td>
+                    <td>{this.props.track.genre}</td>
+                    <td>{this.props.track.year}</td>
+                </tr>
+        } catch (err) {
+            console.error(err)
+        }
+
+        if (track) {
+            return track;
+        } else {
+            return null;
+        }
     }
 };
 
