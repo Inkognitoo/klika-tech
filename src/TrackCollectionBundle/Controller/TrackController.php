7ea3aeb8e4b43c6524bd4787b6ce9175bb04d2ff
Pavel Sotnikov <pavel@alterbooks.ru>
2017-03-06 06:15:17 +0500

    add: комментарии для php-кода

--- src/TrackCollectionBundle/Controller/TrackController.php
+++ src/TrackCollectionBundle/Controller/TrackController.php
@@ -10,6 +10,7 @@ class TrackController extends Controller
 {
     public function getAction(Request $request)
     {
+        //В процессе выполнения скрипта, полностью подготавливаем состояние для клиента
         $em = $this->getDoctrine()->getManager();
         $out = [
             'tracks' => [],
@@ -20,6 +21,7 @@ class TrackController extends Controller
             ],
             'pages' => []
         ];
+        //Собираем для клиента все возможные вариации фильтров
         $singers = $em->getRepository('TrackCollectionBundle:Singer')->findAll();
         $out['filters']['contents']['singers'][] = ['internal_name' => 'all', 'name' => 'Все'];
         $out['filters']['variables']['singer'] = 'all';
@@ -44,20 +46,35 @@ class TrackController extends Controller
         $count = $tracks = $em->createQueryBuilder()->select('COUNT(track)')->from('TrackCollectionBundle:Track', 'track')
             ->getQuery()->getSingleScalarResult();
 
+        /*
+         * Я долго думал, как передавать всё многобразие команд от клиента одним запросом. По логике, запрос - типа GET,
+         * не самый удобный транспорт для множества параметров, особенно ассоциативных массивов. В итоге я решил, что
+         * буду просто отдавать один единственный параметр payload, но содержищай в себе json со всеми необходимыми
+         * командами.
+         */
         $payload = json_decode($request->query->get('payload'), true);
 
         $tracks = $em->createQueryBuilder()->select('track', 'year', 'genre', 'singer')->from('TrackCollectionBundle:Track', 'track')
             ->innerJoin('track.year', 'year')
             ->innerJoin('track.genre', 'genre')
             ->innerJoin('track.singer', 'singer')
-            ->where('1 = 1');
+            ->where('1 = 1'); //это нужно для привязки последующих andWhere()
 
         if (is_array($payload)) {
+            /*
+             * Я решил не разбивать обработку запроса на отдельные методы, так как код получился не слишком длинным,
+             * визуально разделённым и в процессе модифицирующим один и тот же sql запрос
+             */
+
+            /*__ Обработка фильров __________________________________________________________________________________ */
             if (array_key_exists('filters', $payload)) {
                 foreach ($payload['filters'] as $filter) {
                     if (is_array($filter) && array_key_exists('name', $filter) && array_key_exists('value', $filter)) {
                         switch ($filter['name']) {
                             case 'singer':
+                                /* Я не успел найти способа обозначить через symfony базе данных, что internal_name -
+                                 * уникальный индекс, но представим, что он таковым является
+                                 */
                                 $singer = $em->getRepository('TrackCollectionBundle:Singer')
                                     ->findOneBy(['internal_name' => $filter['value']]);
                                 if ($singer) {
@@ -87,12 +104,12 @@ class TrackController extends Controller
                         }
                     }
                 }
-
                 $_count = $tracks->getQuery()
                     ->getResult();
                 $count = count($_count);
             }
 
+            /*__ Обработка сортировок _______________________________________________________________________________ */
             if (array_key_exists('sorts', $payload)) {
                 foreach ($payload['sorts'] as $sort) {
                     if (is_array($sort) && array_key_exists('internal_name', $sort) && array_key_exists('type', $sort)) {
@@ -149,10 +166,16 @@ class TrackController extends Controller
                     }
                 }
             }
+            /*
+             * При отсутствии сортировок, в базе данных postgresql doctrine2 как-то странно себя вёл, как будто бы
+             * запоминая предыдущие сортировки, не знаю с чем конкретно это связано, но сортировка по умолчанию
+             * позволяет этого избежать
+             */
             if(count($out['sorts']) == 0) {
                 $tracks = $tracks->addOrderBy('track.id', 'ASC');
             }
 
+            /*__ Обработка пагинации ________________________________________________________________________________ */
             if (array_key_exists('pages', $payload)) {
                 if (is_array($payload['pages']) && array_key_exists('tracks_count', $payload['pages'])) {
                     $track_count = ((int)$payload['pages']['tracks_count'] > 0) ? (int)$payload['pages']['tracks_count'] : $track_count;
@@ -164,6 +187,7 @@ class TrackController extends Controller
             }
         }
 
+        //Если клиент запрашивает несуществующую страницу, отдаём ему последнюю из имеющихся
         $count = ($count > 0) ? ceil($count / $track_count) : 0;
         if ($current_page > $count) {
             $current_page = $count;
